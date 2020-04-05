4a3a44840194327d52edd88ccb47f137edc48cd6
Kushich <kushich@outlook.com>
2020-04-05 16:44:19 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
 set -e
-# сборка
+
+# build
 npm run build
-# переход в каталог сборки
+
+# navigate into the build output directory
 cd dist
-# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:kushich/v-tasks-app.git master:gh-pages
-cd - 
+
+cd -
